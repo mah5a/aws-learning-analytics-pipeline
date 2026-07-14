29329b57809d21661de149ec00f7c12bce47b593
mah5a <46025103+mah5a@users.noreply.github.com>
2026-07-13 21:19:31 -0700

    Add Athena SQL queries

--- athena/queries.sql
+++ athena/queries.sql
@@ -1,1 +1,22 @@
+-- Number of students
+SELECT COUNT(*) AS total_students
+FROM student_features;
 
+-- Average assessment score
+SELECT AVG(avg_score)
+FROM student_features;
+
+-- Students with failed assessments
+SELECT *
+FROM student_features
+WHERE failed_assessments > 0;
+
+-- Top performing students
+SELECT id_student, avg_score
+FROM student_features
+ORDER BY avg_score DESC
+LIMIT 10;
+
+-- Average submission delay
+SELECT AVG(avg_submission_delay)
+FROM student_features;
